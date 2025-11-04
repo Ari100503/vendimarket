8c064ab3e6b983c2b16b101d9cc881a042d6d6c8
Ari100503 <arianna.parizzi@studbocconi.it>
2025-11-04 21:44:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ class VendimarketApp {
                     }
                 },
                 temp: 'cold',
-                image: 'https://images.unsplash.com/photo-1677177751361-0416255ff002?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070'
+                image: 'https://images.unsplash.com/photo-1677177751361-0416255ff002?w=400&h=300&fit=crop'
             },
             'Water & Hydration': {
                 brands: {
